4cf7c810acfcd5fd7ef2de3420b9d824f0f8d1c7
Obatula Fuad <obatulafuad@gmail.com>
2024-10-16 23:05:08 +0100

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-
 db = SQLAlchemy(app)
+
+#Create function to create app and return the app object
+
+#app = Flask(__name__)
+#config database
+#register blueprints
+#create database
