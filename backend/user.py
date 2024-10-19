cfc6bf8d94816ef4c2b5f6f0957251a9896d2163
Praise Aribisala <aribisalapraise12@gmail.com>
2024-10-19 18:13:32 +0100

    fix hash_password function

--- backend/user.py
+++ backend/user.py
@@ -13,7 +13,8 @@ class User:
         self.phone_number = phone_number
 
     def hash_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        password_hash = generate_password_hash(password)
+        return password_hash
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
